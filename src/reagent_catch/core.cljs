868221623fcd926554218e2f8da53f801991338b
Eugene Potapenko <eugene.john.potapenko@gmail.com>
2019-04-13 10:57:21 +0200

    Format eror stack

--- src/reagent_catch/core.cljs
+++ src/reagent_catch/core.cljs
@@ -1,23 +1,27 @@
 (ns reagent-catch.core
-  (:require  [reagent.core :as r]))
+  (:require  [reagent.core :as r]
+             [clojure.string :as string]))
 
-(defn catch [{:keys [title show-stack?]}]
+(defn catch []
   (let [error-state (r/atom nil)
         info-state  (r/atom nil)]
     (r/create-class
      {:component-did-catch
       (fn [this error info]
         (reset! error-state (str error ))
-        (reset! info-state (-> info .-componentStack)))
+        (reset! info-state (->> info
+                                .-componentStack
+                                (string/split-lines)
+                                (remove string/blank?)
+                                (drop 4)
+                                (take 3)
+                                (string/join "\n"))))
       :reagent-render (fn [& body]
                         (if @error-state
                           [:div
                            {:style {:width "100%"
                                     :backgroundColor "rgba(255,0,0,0.2)"
                                     :padding 8}}
-                           (when title
-                             [:h3 title])
                            [:h6 @error-state]
-                           (when show-stack?
-                             [:pre @info-state])]
+                           [:pre @info-state]]
                           (into [:<>] body)))})))
