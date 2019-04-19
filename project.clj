fa12ee8ec4b82cad3fbba07e3f7a74fa7d5c2b62
Eugene Potapenko <eugene.john.potapenko@gmail.com>
2019-04-19 09:25:58 +0200

    1.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reagent-catch "0.1.0"
+(defproject reagent-catch "0.1.1"
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
