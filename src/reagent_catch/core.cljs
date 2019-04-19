02638f5dec9b0f177fea6707d208cadadd642c2c
Eugene Potapenko <eugene.john.potapenko@gmail.com>
2019-04-19 09:25:58 +0200

    1.0.1

--- src/reagent_catch/core.cljs
+++ src/reagent_catch/core.cljs
@@ -8,20 +8,26 @@
     (r/create-class
      {:component-did-catch
       (fn [this error info]
-        (reset! error-state (str error ))
-        (reset! info-state (->> info
-                                .-componentStack
-                                (string/split-lines)
-                                (remove string/blank?)
-                                (drop 4)
-                                (take 3)
-                                (string/join "\n"))))
+        (reset! error-state (let [max-error-lenght 200]
+                              (when-let [err (str error)]
+                                (if (-> err count < max-error-lenght)
+                                  (-> err
+                                      (subs 0 max-error-lenght)
+                                      (str " ..."))))))
+        (reset! info-state (some->> info
+                                    .-componentStack
+                                    (string/split-lines)
+                                    (remove string/blank?)
+                                    (drop-while #(re-find #"reagent_catch" %))
+                                    (take 3)
+                                    (string/join "\n"))))
       :reagent-render (fn [& body]
                         (if @error-state
                           [:div
-                           {:style {:width "100%"
+                           {:style {:width           "100%"
+                                    :min-width       300
                                     :backgroundColor "rgba(255,0,0,0.2)"
-                                    :padding 8}}
+                                    :padding         8}}
                            [:h6 @error-state]
                            [:pre @info-state]]
                           (into [:<>] body)))})))
