27f54718a81ae2270c09dadf8e45eee0878068ba
Eugene Potapenko <eugene.john.potapenko@gmail.com>
2019-05-03 13:40:03 +0200

    1.1.3

--- src/re_catch/core.cljs
+++ src/re_catch/core.cljs
@@ -2,12 +2,22 @@
   (:require  [reagent.core :as r]
              [clojure.string :as string]))
 
-(defn catch []
+(defn- render-error [{:keys [error info]}]
+  [:div
+   {:style {:width           "100%"
+            :min-width       300
+            :backgroundColor "rgba(255,0,0,0.2)"
+            :padding         8}}
+   [:h6 error]
+   [:pre info]])
+
+(defn catch [{:keys [render-error-component]
+              :or   {render-error-component render-error}}]
   (let [error-state (r/atom nil)
         info-state  (r/atom nil)]
     (r/create-class
      {:component-did-catch
-      (fn [this error info]
+      (fn re-catch-block [this error info]
         (reset! error-state (let [max-error-lenght 200]
                               (when-let [err (str error)]
                                 (if (-> err count (< max-error-lenght))
@@ -18,16 +28,11 @@
                                     .-componentStack
                                     (string/split-lines)
                                     (remove string/blank?)
-                                    (drop-while #(re-find #"G_|re_catch" %))
+                                    (drop-while #(re-find #"re_catch" %))
                                     (take 3)
                                     (string/join "\n"))))
       :reagent-render (fn [& body]
                         (if @error-state
-                          [:div
-                           {:style {:width           "100%"
-                                    :min-width       300
-                                    :backgroundColor "rgba(255,0,0,0.2)"
-                                    :padding         8}}
-                           [:h6 @error-state]
-                           [:pre @info-state]]
+                          [render-error-component
+                           {:error @error-state :info @info-state}]
                           (into [:<>] body)))})))
